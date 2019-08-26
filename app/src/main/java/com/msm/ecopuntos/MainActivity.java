5f6dc60c83f379bc9ed3e297143873371086c0a5
Capacitaciones <capacitaciones@msm.gov.ar>
2019-08-26 11:26:23 -0300

    Pantalla principal y menu

--- app/src/main/java/com/msm/ecopuntos/MainActivity.java
+++ app/src/main/java/com/msm/ecopuntos/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        /*if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/msm/ecopuntos/SplashActivity.java
+++ app/src/main/java/com/msm/ecopuntos/SplashActivity.java
@@ -2,8 +2,10 @@ package com.msm.ecopuntos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
@@ -14,20 +16,50 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-
+        declararControles();
+        configurarControles();
     }
 
     private void declararControles(){
         imgLogo = (ImageView) findViewById(R.id.imgLogo);
     }
 
-    private void configurarControles(){
+    private void configurarControles()
+    {
         AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
         animation.setDuration(2000);
         animation.setStartOffset(1000);
         animation.setFillAfter(true);
-        
+        imgLogo.startAnimation(animation);
 
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Thread timer = new Thread() {
+                    public void run() {
+                        try {
+                            sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            lanzarMenuPrincipal();
+                            finish();
+                        }
+                    }
+                };
+                timer.start();
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+
+    private void lanzarMenuPrincipal() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
